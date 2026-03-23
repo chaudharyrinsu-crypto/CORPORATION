f60e81e0da3bd4121d13efd7a17b3b7ae3138cc1
chaudharyrinsu-crypto <chaudhryrinsu@gmail.com>
2026-03-23 08:28:36 +0545

    changes

--- src/app/products/[id]/components/Pricing.jsx
+++ src/app/products/[id]/components/Pricing.jsx
@@ -8,19 +8,22 @@ const Pricing = ({ product }) => {
             <section className='bg-[#F8F5EE] font-hubot sm:py-20 py-15 lg:py-20 lg:px-0 px-5'>
                 <div className=' lg:max-w-[1100px] mx-auto'>
                     <div>
-                        <h2 className='text-[40px]'>Grow Without Limits: Marketing Hub Pricing</h2>
+                        <h2 className='text-[40px]'>Grow Without Limits: {product.name} Pricing</h2>
                         <p>Marketing software that scales with you.</p>
                     </div>
-                    <div>
-                        <div>
-                            <span className='text-[#FF4800] '></span>
-                            <h3>Free</h3>
-                            <ul>
-                                <li>
-                                    <span></span>
-                                </li>
-                            </ul>
-                        </div>
+                    <div className='grid grid-cols-4'>
+                        {product?.pricingiconsimage?.map((image,index)=>(
+                            <img key={index} src={image} alt="iconimage" />
+                        ))}
+                        {product?.pricingtitle?.map((title,index)=>(
+                            <h2 key={index}>{title}</h2>
+                        ))}
+                        {product?.pricingstatrswith?.map((title,index)=>(
+                            <h2 key={index}>{title}</h2>
+                        ))}
+                        {product?.pricingmonth?.map((month,index)=>(
+                            <h2 key={index}>{month}</h2>
+                        ))}
                     </div>
                 </div>
             </section>
