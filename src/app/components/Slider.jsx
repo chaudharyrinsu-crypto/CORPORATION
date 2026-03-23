ebf667b3a8e6ff3c7511708b1a3940a45b450e56
chaudharyrinsu-crypto <chaudhryrinsu@gmail.com>
2026-03-23 21:16:37 +0545

    changes

--- src/app/components/Slider.jsx
+++ src/app/components/Slider.jsx
@@ -21,57 +21,60 @@ const Slider = () => {
 
     return (
         <>
-            <section className='px-3 text-sm w-full relative '>
-                <div className='flex justify-between px-3'>
-                    <div className='underline underline-offset-8 decoration-2 text-[16px] font-medium  '>NEW ARRIVALS</div>
-                    {/* Custom navigation buttons */}
-                    <div className="md:flex hidden gap-2 items-center text-[18px]">
-                        <button ref={prevRef} disabled={isBeginning} className={`h-9 w-9 rounded-full cursor-pointer flex items-center justify-center shadow ${isBeginning ? 'border border-gray-400 text-gray-400' : 'border border-[#212121] text-[#212121]'}`}>
-                            <LiaAngleLeftSolid />
-                        </button>
-                        <button disabled={isEnd} ref={nextRef} className={`h-9 w-9 rounded-full cursor-pointer flex items-center justify-center shadow ${isEnd ? 'border border-gray-400 text-gray-400' : 'border border-[#212121] text-[#212121]'}`}><LiaAngleRightSolid /></button>
+            <section className=' relative font-hubot'>
+                <div className='max-w-[1100px] mx-auto'>
+                    <div className='px-3'>
+                        {/* Custom navigation buttons */}
+                        <div className="md:flex hidden gap-2 items-center text-[18px]">
+                            <button ref={prevRef} disabled={isBeginning} className={`h-9 w-9 rounded-full cursor-pointer flex items-center justify-center shadow ${isBeginning ? 'border border-gray-400 text-gray-400' : 'border border-[#212121] text-[#212121]'}`}>
+                                <LiaAngleLeftSolid />
+                            </button>
+                            <button disabled={isEnd} ref={nextRef} className={`h-9 w-9 rounded-full cursor-pointer flex items-center justify-center shadow ${isEnd ? 'border border-gray-400 text-gray-400' : 'border border-[#212121] text-[#212121]'}`}><LiaAngleRightSolid /></button>
+                        </div>
                     </div>
-                </div>
-                <div className='pt-4'>
-                    <Swiper
-                        // modules={[Pagination, Navigation]}
-                        modules={[Navigation]}
-                        slidesPerView={1}
-                        pagination={{ clickable: true }}
-                        onBeforeInit={(swiper) => {
-                            swiper.params.navigation.prevEl = prevRef.current
-                            swiper.params.navigation.nextEl = nextRef.current
-                        }}
-                        onSwiper={(swiper) => {
-                            setIsBeginning(swiper.isBeginning)
-                            setIsEnd(swiper.isEnd)
-                        }}
-                        onSlideChange={(swiper) => {
-                            setIsBeginning(swiper.isBeginning)
-                            setIsEnd(swiper.isEnd)
-                        }}
-                        navigation={{
-                            prevEl: prevRef.current,
-                            nextEl: nextRef.current,
-                        }}
-                        className="w-full h-full new-arrival-swiper "
-                    >
-                        {slider.map((item, i) => (
-                            // newArrivashoeItem
-                            <SwiperSlide
-                                key={i}
-                                className="flex items-center justify-center bg-white rounded-2xl py-5 "
-                            >
-                                <div className='flex'>
-                                    <div className='px-5 space-y-2'>
-                                        <h3 className='uppercase text-[14px]'>{item.title}</h3>
-                                        <p>{item.description}</p>
+                    <div className='pt-4'>
+                        <Swiper
+                            // modules={[Pagination, Navigation]}
+                            modules={[Navigation]}
+                            slidesPerView={1}
+                            pagination={{ clickable: true }}
+                            onBeforeInit={(swiper) => {
+                                swiper.params.navigation.prevEl = prevRef.current
+                                swiper.params.navigation.nextEl = nextRef.current
+                            }}
+                            onSwiper={(swiper) => {
+                                setIsBeginning(swiper.isBeginning)
+                                setIsEnd(swiper.isEnd)
+                            }}
+                            onSlideChange={(swiper) => {
+                                setIsBeginning(swiper.isBeginning)
+                                setIsEnd(swiper.isEnd)
+                            }}
+                            navigation={{
+                                prevEl: prevRef.current,
+                                nextEl: nextRef.current,
+                            }}
+                            className="w-full h-full new-arrival-swiper "
+                        >
+                            {slider.map((item, i) => (
+                                // newArrivashoeItem
+                                <SwiperSlide
+                                    key={i}
+                                    className="flex items-center justify-center  py-5 "
+                                >
+                                    <div className='grid grid-cols-1 2md:grid-cols-2 items-center gap-10'>
+                                        <div className='px-5 space-y-5 2md:order-1 order-2'>
+                                            <h3 className='leading-11 font-medium md:text-[32px] text-[24px] lg:text-[40px]'>{item.title}</h3>
+                                            <p>{item.description}</p>
+                                        </div>
+                                        <div className='2md:order-2 order-1 '>
+                                        <img className='w-full max-h-[400px] object-contain' src={item.image} alt="image" />
+                                        </div>
                                     </div>
-                                    <img className='object-cover h-full w-full' src={item.image} alt="image" />
-                                </div>
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+                    </div>
                 </div>
             </section>
         </>
