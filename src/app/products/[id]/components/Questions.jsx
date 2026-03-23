73757310735e747a4386785b3aef41a875259a6c
chaudharyrinsu-crypto <chaudhryrinsu@gmail.com>
2026-03-23 11:15:19 +0545

    changes

--- src/app/products/[id]/components/Questions.jsx
+++ src/app/products/[id]/components/Questions.jsx
@@ -5,12 +5,12 @@ import { LiaAngleDownSolid } from "react-icons/lia";
 const Questions = ({product}) => {
   return (
     <>
-      <section className='font-hubot bg-[#FCFCFA]'>
+      <section className='font-hubot bg-[#FCFCFA] py-20'>
         <div className=' lg:max-w-[1100px] mx-auto font-semibold'>
-            <h2 className='text-2xl'>Frequently Asked Questions</h2>
-            <div className='grid grid-cols-2 gap-5'>
+            <h2 className='text-2xl pb-10'>Frequently Asked Questions</h2>
+            <div className='grid grid-cols-2 justify-between gap-5'>
             {product.questions?.map((question,index)=>(
-                <div className='text-[18px] border border-[#D9D9D8] rounded-xl p-5 flex justify-between '>
+                <div className='text-[18px] border border-[#D9D9D8] rounded-2xl p-5 flex justify-between '>
                     <h3>{question}</h3>
                     <span><LiaAngleDownSolid /></span>
                 </div>
