1f0a90bcd50576510546c58e9eb719a8dc34bb54
chaudharyrinsu-crypto <chaudhryrinsu@gmail.com>
2026-03-24 10:53:10 +0545

    changes

--- src/app/components/ProductList.jsx
+++ src/app/components/ProductList.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { products } from '../data/products'
 import { GoCheck } from "react-icons/go";
 import Link from 'next/link';
+import { GoArrowRight } from "react-icons/go";
 
 const ProductList = () => {
     return (
         <>
             <div className='grid grid-cols-2 gap-6'>
                 {products.map((item, i) => (
-                    <Link href={`/products/${item.id}`} key={i} className='border border-[#D9D9D8] rounded-2xl p-7'>
+                    <Link href={`/products/${item.id}`} key={i} className='border border-[#D9D9D8] rounded-2xl p-7 group'>
                         <div className='flex gap-2 border-b border-dashed border-[#D9D9D8] pb-4'>
                             <img className='h-6' src={item.logo} alt="logo" />
                             <h3 className='font-semibold'>{item.heading}</h3>
@@ -20,11 +21,12 @@ const ProductList = () => {
                                         <span className='text-[18px] text-[#FF4800]'><GoCheck /></span>
                                         <p className='text-sm'>{feature}</p>
                                     </div>
-                                    <div>
-                                        
-                                    </div>
                                 </div>
                             ))}
+                        </div>
+                        <div className='flex items-center gap-2 font-medium'>
+                            <span className='text-sm'>Learn more</span>
+                            <span className='group-hover:translate-x-1 duration-300'><GoArrowRight /></span>
                         </div>
                     </Link>
                 ))}
