f9e9bcb3eb0293b9a58b97a15d679350cf718680
chaudharyrinsu-crypto <chaudhryrinsu@gmail.com>
2026-03-24 10:58:56 +0545

    changes

--- src/app/components/ProductList.jsx
+++ src/app/components/ProductList.jsx
@@ -2,31 +2,31 @@ import React from 'react'
 import { products } from '../data/products'
 import { GoCheck } from "react-icons/go";
 import Link from 'next/link';
-import { GoArrowRight } from "react-icons/go";
+import { HiArrowRight } from "react-icons/hi2";
 
 const ProductList = () => {
     return (
         <>
             <div className='grid grid-cols-2 gap-6'>
                 {products.map((item, i) => (
-                    <Link href={`/products/${item.id}`} key={i} className='border border-[#D9D9D8] rounded-2xl p-7 group'>
+                    <Link href={`/products/${item.id}`} key={i} className='border border-[#D9D9D8] rounded-2xl p-7 group hover:bg-[#eeeeed] duration-300'>
                         <div className='flex gap-2 border-b border-dashed border-[#D9D9D8] pb-4'>
                             <img className='h-6' src={item.logo} alt="logo" />
                             <h3 className='font-semibold'>{item.heading}</h3>
                         </div>
-                        <div className='border-b border-dashed border-[#D9D9D8]'>
+                        <div className='border-b border-dashed border-[#D9D9D8] py-4'>
                             {item.features?.map((feature, index) => (
-                                <div key={index} className='rounded-2xl'>
-                                    <div className='flex gap-2 tracking-wide py-1  '>
+                                <div key={index} className=''>
+                                    <div className='flex gap-2 tracking-wide'>
                                         <span className='text-[18px] text-[#FF4800]'><GoCheck /></span>
                                         <p className='text-sm'>{feature}</p>
                                     </div>
                                 </div>
                             ))}
                         </div>
-                        <div className='flex items-center gap-2 font-medium'>
+                        <div className='flex items-center gap-2 font-medium pt-4'>
                             <span className='text-sm'>Learn more</span>
-                            <span className='group-hover:translate-x-1 duration-300'><GoArrowRight /></span>
+                            <span className='group-hover:translate-x-1.5 duration-300 font-normal'><HiArrowRight /></span>
                         </div>
                     </Link>
                 ))}
